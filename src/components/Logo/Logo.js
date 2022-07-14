a5efb834412fd9f7bcc6ce4befae933d11dd3962
reed shipley <reedbshipley@gmail.com>
2022-07-14 12:51:40 -0600

    img
    Signed-off-by: reed shipley <reedbshipley@gmail.com>

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,6 +5,8 @@ const Logo = (props) => {
       src={require("../../assets/Logo.png")}
       className={props.classStyle || 'defaultLogo'}
       alt="Conscious Logo"
+      height={props.height || 253}
+      width={props.width || 54}
     >
     </img>
   )
